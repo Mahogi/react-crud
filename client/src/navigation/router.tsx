34c746b2d16fb54c2561628b2011d07055c29980
Mahogi <milda.gedvilaite@gmail.com>
2023-02-13 22:01:33 +0200

    (feat): Created house cards and styled them

--- client/src/navigation/router.tsx
+++ client/src/navigation/router.tsx
@@ -3,11 +3,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import NavbarLayout from 'components/layout/navbar-layout';
 import HomePage from 'pages/home-page';
 import routes from './routes';
+import HouseListPage from '../pages/house-list-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavbarLayout />,
+    // element: <NavbarLayout />,
     children: [
       {
         path: routes.HomePage,
--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import ApiService from 'services/api-service';
+import classes from './index.module.css';
+import HouseCard from './house-list-page/house-card';
 
 const HomePage = () => {
   const [houses, setHouses] = React.useState<HouseModel[]>([]);
+  const title = 'Vacation Earth Homes';
 
   React.useEffect(() => {
     (async () => {
@@ -13,11 +16,13 @@ const HomePage = () => {
   }, []);
 
   return (
-    <Box>
-      <Box component="pre">
-        {JSON.stringify(houses, null, 4)}
+    <>
+      <h1 className={classes.h1}>{title}</h1>
+      <Box className={classes.container}>
+        {houses.map((house) => (<HouseCard key={house.id} {...house} />
+        ))}
       </Box>
-    </Box>
+    </>
   );
 };
 
