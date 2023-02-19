7c4a8b7929a89dc16f8727ff860c964e350286b8
Mahogi <milda.gedvilaite@gmail.com>
2023-02-19 15:20:46 +0200

    (feat): Added create form UI

--- client/src/pages/house-form-page/index.tsx
+++ client/src/pages/house-form-page/index.tsx
@@ -1,14 +1,96 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box, Button, Rating, Stack, TextField, Typography,
+} from '@mui/material';
+import LocationField from './location-field';
+import ImagesField from './images-field';
+import * as Styled from './styled';
+
+const formatValues = (form: HTMLFormElement) => {
+  const formData = new FormData(form);
+
+  const title = formData.get('title');
+  const price = formData.get('price');
+  const rating = formData.get('rating');
+  const images = formData.getAll('images');
+  const country = formData.get('country');
+  const city = formData.get('city');
+
+  if (title === null || title instanceof File || title.length < 2) throw new Error('incorrect Title');
+  if (price === null || price instanceof File || price.length < 1) throw new Error('incorrect Price');
+  if (rating === null || rating instanceof File || rating.length < 1) throw new Error('incorrect Rating');
+  if (country === null || country instanceof File || country.length < 2) throw new Error('incorrect Country');
+  if (city === null || city instanceof File || city.length < 2) throw new Error('incorrect City');
+  images.forEach((img, i) => {
+    if (img instanceof File || img.length < 2) throw new Error(`incorrect Image nr: ${i + 1}`);
+  });
+
+  return {
+    title,
+    location: {
+      country,
+      city,
+    },
+    images: images as string[],
+    price: `${Number(price).toFixed(2)}€`,
+    rating: Number(rating),
+  };
+};
 
 type HouseFormPageProps = {
   mode?: 'create' | 'edit'
 };
 
 const HouseFormPage: React.FC<HouseFormPageProps> = ({ mode = 'create' }) => {
-  const test = 'HouseFormPage';
+  const formRef = React.useRef<HTMLFormElement | null>(null);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    if (formRef.current === null) return;
+
+    try {
+      const values = formatValues(formRef.current);
+      console.log(values);
+    } catch (error) {
+      alert(error instanceof Error ? error.message : error);
+    }
+  };
   return (
-    <Box>{mode}</Box>
+    <Styled.Container>
+      <Styled.PaperForm elevation={4} onSubmit={handleSubmit} ref={formRef}>
+        <Typography variant="h4" sx={{ textAlign: 'center' }}>Create House</Typography>
+        <Stack sx={{ gap: 2, mt: 2 }}>
+          <TextField label="Title" fullWidth variant="filled" name="title" required />
+          <LocationField />
+          <ImagesField />
+
+          <TextField
+            label="Price"
+            fullWidth
+            variant="filled"
+            name="price"
+            type="number"
+            inputProps={{ step: '0.01' }}
+            required
+          />
+          <Box>
+            <Typography component="legend">Rating</Typography>
+            <Rating name="rating" />
+          </Box>
+
+          <Stack alignItems="center" sx={{ mt: 2 }}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              Create
+            </Button>
+          </Stack>
+        </Stack>
+      </Styled.PaperForm>
+    </Styled.Container>
   );
 };
 
